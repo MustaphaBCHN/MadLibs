fd3bbc0be61da0fa48e304f42556d0703f9f7135
MustaphaBCHN <bouchenemustapha@gmail.com>
2022-07-11 10:36:44 +0100

    fixed some bugs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let originalStory= 'My three-year-old daughter was trying to roast a marshmallow
 let userInputes = [];
 
 
-console.log (`My ${userInputes[3]}-year-old ${userInputes[1]} was trying to roast a ${userInputes[4]} for the first time. Her first and second attempts ended in flames. Both times, I took the scorched ${userInputes[4]} off of the roasting stick and threw it into the fire. The third time, I helped her a lot more and, together, we achieved a perfectly toasty ${UserInputs[5]} ${UserInputs[6]}. Once it was cool, I handed her the ${userInputes[4]} which she promptly threw into the fire. No one had told her she was supposed to ${userInputes[2]} it.`);
+console.log (`My ${userInputes[3]}-year-old ${userInputes[1]} was trying to roast a ${userInputes[4]} for the first time. Her first and second attempts ended in flames. Both times, I took the scorched ${userInputes[4]} off of the roasting stick and threw it into the fire. The third time, I helped her a lot more and, together, we achieved a perfectly toasty ${userInputes[5]} ${userInputes[6]}. Once it was cool, I handed her the ${userInputes[4]} which she promptly threw into the fire. No one had told her she was supposed to ${userInputes[2]} it.`);
 
 // 1 noun   2 eat   3 three    4 marshmallow   5 golden  6 brown
 let numberOfQuestions = 6
